5b7d78f88bfc32bf3bd43b35ce5dec25365b8e95
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-06-06 10:44:35 +0430

    Index.js updated for luxon error handling

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import initializeNavigation from './modules/navigation.js';
 import Library from './modules/libray.js';
 import currentDateAndTime from './modules/luxon.js';
 
+currentDateAndTime();
 const subLibrary = new Library();
 subLibrary();
 initializeNavigation();
-currentDateAndTime();
--- modules/luxon.js
+++ modules/luxon.js
@@ -1,9 +1,7 @@
-import { DateTime } from '../node_modules/luxon/src/luxon.js';
-
 const currentDateAndTime = () => {
-  const currentTime = DateTime.now().toLocaleString(DateTime.DATETIME_FULL);
   const datePage = document.querySelector('.date-page');
-  datePage.textContent = currentTime;
+  const newdate = new Date();
+  datePage.textContent = newdate.toDateString();
 };
 
 export default currentDateAndTime;
